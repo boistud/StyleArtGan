9910642165dfae80c4a0aa742ada6bba2edc751a
boistud <57449107+boistud@users.noreply.github.com>
2019-11-28 20:45:25 -0500

    Update convert_to_tfr.py

--- stylegan-master/convert_to_tfr.py
+++ stylegan-master/convert_to_tfr.py
@@ -1,3 +1,5 @@
+# The directories only exist on the VM since images are too big to push on git
+
 import dataset_tool as dt
 
 dt.create_from_images('..//..//wikiart-master/wikiart-saved/subset_images_tfr//', '..//..//wikiart-master//wikiart-saved//subset_images_resized', True)
