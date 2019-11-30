a4460daa9870bbd1de46e4cc31ec807cb088dd06
boistud <57449107+boistud@users.noreply.github.com>
2019-11-30 13:35:18 -0500

    Update convert_to_tfr.py

--- stylegan-master/convert_to_tfr.py
+++ stylegan-master/convert_to_tfr.py
@@ -1,5 +1,6 @@
 # The directories only exist on the VM since images are too big to push on git
-
+# Convert images to tfrecords and add labels(style+genre) as well
 import dataset_tool as dt
 
-dt.create_from_images('..//..//wikiart-master/wikiart-saved/subset_images_tfr//', '..//..//wikiart-master//wikiart-saved//subset_images_resized', True)
+dt.create_from_images('..//..//StyleArtGan-master//stylegan-master//datasets//', '..//..//wikiart-master//wikiart-saved//subset_images_resized', True)
+
