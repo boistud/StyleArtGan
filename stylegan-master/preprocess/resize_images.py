29ff6a11da97c679924ec669018e58c75f0aacfc
boistud <57449107+boistud@users.noreply.github.com>
2019-11-30 16:15:26 -0500

    generalize to a class

--- stylegan-master/preprocess/resize_images.py
+++ stylegan-master/preprocess/resize_images.py
@@ -1,15 +1,20 @@
 from PIL import Image
 import os
-directories = []
-class_names = []
-for filename in os.listdir("subset_images2"):
-	path = os.path.join("subset_images2",filename)
-	#print(path)
-	foo = Image.open(path)
-	if foo.mode != 'RGB':
-		foo = foo.convert('RGB')
-	foo = foo.resize((512,512),Image.ANTIALIAS)
 
-	new_path = "subset_images_resized"
-	save_path = os.path.join(new_path,os.path.basename(path))
-	foo.save(save_path,quality=95)
+class Converter:
+	def __init__(self, image_resolution, file):
+		self.image_resolution = image_resolution # e.g. (512,512)
+		self.file = file
+		
+	def convert():
+		for filename in os.listdir(file):
+			path = os.path.join(file,filename)
+			
+			foo = Image.open(path)
+			if foo.mode != 'RGB':
+				foo = foo.convert('RGB')
+			foo = foo.resize(self.image_resolution,Image.ANTIALIAS)
+
+			new_path = "subset_images_resized"
+			save_path = os.path.join(new_path,os.path.basename(path))
+			foo.save(save_path,quality=95)
