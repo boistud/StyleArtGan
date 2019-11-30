2944a9aff569301aaa3a8fa2279adab0cb489701
boistud <57449107+boistud@users.noreply.github.com>
2019-11-30 16:17:56 -0500

    Update convert_to_tfr.py

--- stylegan-master/convert_to_tfr.py
+++ stylegan-master/convert_to_tfr.py
@@ -1,6 +1,11 @@
 # The directories only exist on the VM since images are too big to push on git
 # Convert images to tfrecords and add labels(style+genre) as well
 import dataset_tool as dt
+import preprocess
+
+
+corvett = Converter(image_resolution=(512,512),file='subset_images2')
+corvett.convert()
 
 dt.create_from_images('..//..//StyleArtGan-master//stylegan-master//datasets//subset_images_tfr', '..//..//wikiart-master//wikiart-saved//subset_images_resized', True)
 
