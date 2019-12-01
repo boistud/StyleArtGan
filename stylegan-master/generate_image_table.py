a051ba1243de14c0add6e82028317dc2e4ff0b74
joshwende <57448885+joshwende@users.noreply.github.com>
2019-11-30 23:10:55 -0500

    big fixes

--- stylegan-master/generate_image_table.py
+++ stylegan-master/generate_image_table.py
@@ -15,8 +15,10 @@ def generate_image(pickle_file, styles, genres, output_file, n_images=1):
 	tflib.init_tf()
 
 	# map the features to their label indices
-	styles_to_idx = {'impressionism' = 0, 'cubism' = 1, 'analytical cubism' = 1, 'expressionism' = 2, 'symbolism' = 2, 'surrealism' = 3, 'high renaissance' = 4}
-	genres_to_idx = {'portrait' = 5, 'landscape' = 6, 'sculpture' = 7, 'genre painting' = 8}
+	styles_to_idx = {'impressionism': 0, 'cubism': 1, 'analytical cubism': 1, 'expressionism': 2, 'symbolism': 2, 'surrealism': 3, 'high renaissance': 4}
+	genres_to_idx = {'portrait': 5, 'landscape': 6, 'sculpture': 7, 'genre painting': 8}
+
+	max_index = 8
 
 	# load the model
 	f = open(pickle_file,"rb")
@@ -25,10 +27,11 @@ def generate_image(pickle_file, styles, genres, output_file, n_images=1):
 	_G, _D, Gs = pickle.load(sio)
 
 	# generate the random input vectors
+	rnd = np.random.RandomState(5)
 	latents = rnd.randn(n_images, Gs.input_shape[1])
 
 	# initialize the label vector
-	label = [0] * (len(styles_to_idx) + len(genres_to_idx))
+	label = [0] * (max_index + 1)
 
 	# for each style
 	for style in styles:
@@ -85,8 +88,10 @@ def generate_image_grid(pickle_file, output_file, styles=None, genres=None):
 	all_styles = ['impressionism', 'cubism', 'analytical cubism', 'expressionism', 'symbolism', 'surrealism', 'high renaissance']
 	all_genres = ['portrait', 'landscape', 'sculpture', 'genre painting']
 
-	styles_to_idx = {'impressionism' = 0, 'cubism' = 1, 'analytical cubism' = 1, 'expressionism' = 2, 'symbolism' = 2, 'surrealism' = 3, 'high renaissance' = 4}
-	genres_to_idx = {'portrait' = 5, 'landscape' = 6, 'sculpture' = 7, 'genre painting' = 8}
+	styles_to_idx = {'impressionism': 0, 'cubism': 1, 'analytical cubism': 1, 'expressionism': 2, 'symbolism': 2, 'surrealism': 3, 'high renaissance': 4}
+	genres_to_idx = {'portrait': 5, 'landscape': 6, 'sculpture': 7, 'genre painting': 8}
+
+	max_index = 8
 
 	f = open(pickle_file,"rb")
 	bin_data = f.read()
@@ -94,6 +99,7 @@ def generate_image_grid(pickle_file, output_file, styles=None, genres=None):
 	_G, _D, Gs = pickle.load(sio)
 
 	# generate the random input vectors
+	rnd = np.random.RandomState(5)
 	latents = rnd.randn(images_per_combo, Gs.input_shape[1])
 
 	fmt = dict(func=dnnlib.tflib.convert_images_to_uint8, nchw_to_nhwc=True)
@@ -109,6 +115,8 @@ def generate_image_grid(pickle_file, output_file, styles=None, genres=None):
 
 				return
 
+		all_styles = styles
+
 	# check any specified genres too
 	if genres != None:
 
@@ -120,64 +128,61 @@ def generate_image_grid(pickle_file, output_file, styles=None, genres=None):
 
 				return
 
-	# if left as None, do all of them
-	if styles is None:
+		all_genres = genres
 
-		# this will be the final image
-		grid = None
+	# this will be the final image
+	grid = None
 
-		for style in all_styles:
+	for style in all_styles:
 
-			# this will be all the images for this style concatenated together
-			row = None
+		# this will be all the images for this style concatenated together
+		row = None
 
-			if genres is None:
+		for genre in all_genres:
 
-				for genre in all_genres:
+			# initialize the label
+			label = [0] * (max_index + 1)
 
-					# initialize the label
-					label = [0] * (len(styles_to_idx) + len(genres_to_idx))
+			# set the label
+			label[styles_to_idx[style.lower()]] = 1
+			label[genres_to_idx[genre.lower()]] = 1
 
-					# set the label
-					label[styles_to_idx[style.lower()]] = 1
-					label[genres_to_idx[genre.lower()]] = 1
+			# duplicate the label as needed
+			labels = [label] * images_per_combo
 
-					# duplicate the label as needed
-					labels = [label] * images_per_combo
+			# generate the images
+			images = Gs.run(latents, labels, truncation_psi=0.7, randomize_noise=True, output_transform=fmt)
 
-					# generate the images
-					images = Gs.run(latents, labels, truncation_psi=0.7, randomize_noise=True, output_transform=fmt)
+			# format the grid and the images in it
+			side_length = np.sqrt(images_per_combo)
 
-					# format the grid and the images in it
-					side_length = np.sqrt(images_per_combo)
+			# make the first row of this output
+			output = np.concatenate((images[:side_length]), axis=1)
 
-					# make the first row of this output
-					output = np.concatenate((images[:side_length]), axis=1)
+			# concatenate the rest of the rows
+			for i in range(1, side_length):
 
-					# concatenate the rest of the rows
-					for i in range(1, side_length):
+				output = np.concatenate((output, np.concatenate((images[i * side_length: (i + 1) * side_length]), axis=1)), axis=0)
 
-						output = np.concatenate((output, np.concatenate((images[i * side_length: (i + 1) * side_length]), axis=1)), axis=0)
+			# add this output to the row
+			if row is None:
 
-					# add this output to the row
-					if row is None:
-
-						row = output.copy()
-
-					else:
-
-						row = np.concatenate((row, output), axis=1)
-
-			# add this row to the grid
-			if grid is None:
-
-				grid = row.copy()
+				row = output.copy()
 
 			else:
 
-				grid = np.concatenate((grid, row), axis=0)
+				row = np.concatenate((row, output), axis=1)
 
-		# save the image
-		PIL.Image.fromarray(grid, 'RGB').save(output_file)
+		# add this row to the grid
+		if grid is None:
+
+			grid = row.copy()
+
+		else:
+
+			grid = np.concatenate((grid, row), axis=0)
+
+	# save the image
+	PIL.Image.fromarray(grid, 'RGB').save(output_file)
 
 generate_image_grid('results/00015-sgan-sampleset-cond-1gpu-tuned-baseline-add-mapping-and-styles-remove-traditional-input-add-noise-inputs-stylebased-2/network-snapshot-006126.pkl', output_file='grid.png')
