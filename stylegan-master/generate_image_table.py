fb1706c4beaf3c876f0be25cf8993101a9c8e397
joshwende <57448885+joshwende@users.noreply.github.com>
2019-12-01 18:54:22 -0500

    Add files via upload

--- stylegan-master/generate_image_table.py
+++ stylegan-master/generate_image_table.py
@@ -80,13 +80,16 @@ def generate_image(pickle_file, styles, genres, output_file, n_images=1, seed=No
 	# save the image to file
 	PIL.Image.fromarray(output, 'RGB').save(output_file)
 
-def generate_style_mix(pickle_file, fine_style, fine_genre, coarse_style, coarse_genre, output_file, n_images=1, seed=None):
+# generates images with style mixing from specified artistic styles and genres
+def generate_style_mix(pickle_file, fine_style, fine_genre, coarse_style, coarse_genre, output_file, n_images=1, seed=100):
 
 	tflib.init_tf()
 
+	# dictionaries for building the labels
 	styles_to_idx = {'impressionism': 0, 'cubism': 1, 'analytical cubism': 1, 'expressionism': 2, 'symbolism': 2, 'surrealism': 3, 'high renaissance': 4}
 	genres_to_idx = {'portrait': 5, 'landscape': 6, 'sculpture': 7, 'genre painting': 8}
 
+	# the max index in the dictionaries
 	max_index = 8
 
 	# load the model
@@ -101,9 +104,11 @@ def generate_style_mix(pickle_file, fine_style, fine_genre, coarse_style, coarse
 	rnd = np.random.RandomState(seed * 2)
 	coarse_latents = rnd.randn(n_images, Gs.input_shape[1])
 
+	# initialize the labels
 	fine_label = [0] * (max_index + 1)
 	coarse_label = [0] * (max_index + 1)
 
+	# check that the specified styles and genres are valid
 	if fine_style.lower() not in styles_to_idx:
 
 		print('Error: \'' + str(fine_style) + '\' not a valid style')
@@ -128,38 +133,49 @@ def generate_style_mix(pickle_file, fine_style, fine_genre, coarse_style, coarse
 
 		return
 
+	# set the labels
 	fine_label[styles_to_idx[fine_style.lower()]] = 1
 	fine_label[genres_to_idx[fine_genre.lower()]] = 1
 	coarse_label[styles_to_idx[coarse_style.lower()]] = 1
 	coarse_label[genres_to_idx[coarse_genre.lower()]] = 1
 
+	# duplicate the labels as needed
 	fine_labels = [fine_label] * n_images
 	coarse_labels = [coarse_label] * n_images
 
+	# pass the labels and latents through the mapping network
 	fine_dlatents = Gs.components.mapping.run(fine_latents, fine_labels)
 	coarse_dlatents = Gs.components.mapping.run(coarse_latents, coarse_labels)
 
 	fmt = dict(func=dnnlib.tflib.convert_images_to_uint8, nchw_to_nhwc=True)
 
+	# generate the images whose styles are to be mixed
 	fine_images = Gs.components.synthesis.run(fine_dlatents, truncation_psi=0.7, randomize_noise=True, output_transform=fmt)
 	coarse_images = Gs.components.synthesis.run(coarse_dlatents, truncation_psi=0.7, randomize_noise=True, output_transform=fmt)
 
+	# initialize the mixed dlatents list
 	mixed_dlatents = []
 
+	# for each pair of images
 	for i in range(len(fine_images)):
 
+		# take the course dlatents
 		mixed_dlatents.append(coarse_dlatents[i].copy())
 
-		mixed_dlatents[i][int(len(mixed_dlatents[i]) / 2):] = fine_dlatents[i][int(len(mixed_dlatents[i]) / 2):]
+		# and copy the fine dlatents over the second half
+		mixed_dlatents[i][0][int(len(mixed_dlatents[i][0]) / 2):] = fine_dlatents[i][0][int(len(mixed_dlatents[i][0]) / 2):]
 
-	mixed_images = Gs.components.synthesis.run(mixed_dlatents, truncation_psi=0.7, randomize_noise=True, output_transform=fmt)
+	# generate the style-mixed images
+	mixed_images = Gs.components.synthesis.run(np.array(mixed_dlatents), truncation_psi=0.7, randomize_noise=True, output_transform=fmt)
 
+	# concatenate the images into a nice figure
 	figure = np.concatenate(([np.concatenate((coarse_images), axis=1), np.concatenate((mixed_images), axis=1), np.concatenate((fine_images), axis=1)]), axis=0)
 
+	# save the figure
 	PIL.Image.fromarray(figure, 'RGB').save(output_file)
 
 # generates images for every style/genre combo and saves them all as one tiled image where each row is a style and each column is a genre
-def generate_image_grid(pickle_file, output_file, styles=None, genres=None, seed=None):
+def generate_image_grid(pickle_file, output_file, styles=None, genres=None, seed=100):
 
 	tflib.init_tf()
 
@@ -182,10 +198,6 @@ def generate_image_grid(pickle_file, output_file, styles=None, genres=None, seed
 	bin_data = f.read()
 	sio = BytesIO(bin_data)
 	_G, _D, Gs = pickle.load(sio)
-
-	# generate the random input vectors
-	rnd = np.random.RandomState(seed)
-	latents = rnd.randn(images_per_combo, Gs.input_shape[1])
 
 	fmt = dict(func=dnnlib.tflib.convert_images_to_uint8, nchw_to_nhwc=True)
 
@@ -222,6 +234,8 @@ def generate_image_grid(pickle_file, output_file, styles=None, genres=None, seed
 
 	row_number = 0
 
+	k = 1
+
 	for style in all_styles:
 
 		# this will be all the images for this style concatenated together
@@ -240,6 +254,12 @@ def generate_image_grid(pickle_file, output_file, styles=None, genres=None, seed
 
 			# duplicate the label as needed
 			labels = [label] * images_per_combo
+
+			# generate the random input vectors
+			rnd = np.random.RandomState(seed * k)
+			latents = rnd.randn(images_per_combo, Gs.input_shape[1])
+
+			k += 1
 
 			# generate the images
 			images = Gs.run(latents, labels, truncation_psi=0.7, randomize_noise=True, output_transform=fmt)
@@ -292,6 +312,6 @@ def generate_image_grid(pickle_file, output_file, styles=None, genres=None, seed
 	# save the image
 	PIL.Image.fromarray(grid, 'RGB').save(output_file)
 
-#generate_image_grid('results/00015-sgan-sampleset-cond-1gpu-tuned-baseline-add-mapping-and-styles-remove-traditional-input-add-noise-inputs-stylebased-2/network-snapshot-006126.pkl', output_file='grid.png', genres=['portrait', 'landscape'])
+generate_image_grid('results/00015-sgan-sampleset-cond-1gpu-tuned-baseline-add-mapping-and-styles-remove-traditional-input-add-noise-inputs-stylebased-2/network-snapshot-006126.pkl', output_file='grid.png', genres=['portrait', 'landscape'], seed=5)
 
-generate_style_mix('results/00015-sgan-sampleset-cond-1gpu-tuned-baseline-add-mapping-and-styles-remove-traditional-input-add-noise-inputs-stylebased-2/network-snapshot-006126.pkl', fine_style='impressionism', fine_genre='landscape', coarse_style='cubism', coarse_genre='portrait', output_file='mixing.png', n_images=8, seed=100)
+generate_style_mix('results/00015-sgan-sampleset-cond-1gpu-tuned-baseline-add-mapping-and-styles-remove-traditional-input-add-noise-inputs-stylebased-2/network-snapshot-006126.pkl', fine_style='impressionism', fine_genre='landscape', coarse_style='cubism', coarse_genre='portrait', output_file='mixing.png', n_images=8, seed=6)
