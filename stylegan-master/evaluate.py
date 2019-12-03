917be8cf1da5abbd67b8ff7d23aeeb5690068108
boistud <57449107+boistud@users.noreply.github.com>
2019-12-03 00:04:38 -0500

    Update evaluate.py

--- stylegan-master/evaluate.py
+++ stylegan-master/evaluate.py
@@ -15,24 +15,26 @@ from training import dataset
 # Evaluate a network by FID score
 
 # Directory of the pickled model saved under the folder 'results'
+
+def evaluate(dir):
+  
+  tflib.init_tf()
+
+  f = open(dir + 'network-final.pkl',"rb")
+  bin_data = f.read()
+  sio = BytesIO(bin_data)
+  _G, _D, Gs = pickle.load(sio)
+
+  #fid = dnnlib.EasyDict(func_name='metrics.frechet_inception_distance.FID', name='fid50k', num_images=2788, minibatch_per_gpu=8)
+
+  #dataset = EasyDict(tfrecord_dir='subset_images_tfr')
+
+  evaluater = fid.FID(num_images=2788, minibatch_per_gpu=8, name='fid')
+  #run_config = misc.parse_config_for_previous_run(dir)
+  #print(run_config)
+  #evaluater.run = dict(run_config['dataset'])
+  evaluater.run(dir + 'network-final.pkl', run_dir=dir, dataset_args=None, mirror_augment=True, num_gpus=1)
+  evaluater._evaluate(Gs, 1)
+
 dir = 'results/00005-sgan-sampleset-cond-1gpu-tuned-baseline-add-mapping-and-styles-remove-traditional-input-add-noise-inputs-stylebased-2/'
-
-tflib.init_tf()
-
-f = open(dir + 'network-final.pkl',"rb")
-bin_data = f.read()
-sio = BytesIO(bin_data)
-_G, _D, Gs = pickle.load(sio)
-
-#fid = dnnlib.EasyDict(func_name='metrics.frechet_inception_distance.FID', name='fid50k', num_images=2788, minibatch_per_gpu=8)
-
-dataset = EasyDict(tfrecord_dir='subset_images_tfr')
-
-evaluater = fid.FID(num_images=2788, minibatch_per_gpu=8, name='fid')
-#run_config = misc.parse_config_for_previous_run(dir)
-#print(run_config)
-#evaluater.run = dict(run_config['dataset'])
-evaluater.run(dir + 'network-final.pkl', run_dir=dir, dataset_args=None, mirror_augment=True, num_gpus=1)
-evaluater._evaluate(Gs, 1)
-
-
+evaluate(dir)
